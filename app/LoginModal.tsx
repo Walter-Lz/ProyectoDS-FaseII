1c7936eb6cdebef3ff27fb8d5ecf3eac2deeee1f
leiner alvarado <leineralvarador117@gmail.com>
2024-09-24 20:38:34 -0600

    Refactor sign-in function to use async/await syntax

---
+++ app/LoginModal.tsx
@@ -0,0 +1,149 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Modal, Image } from 'react-native';
+import { signInWithGoogle,logOut } from '../config/firebaseConfig';
+
+const LoginModal = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado para verificar si el usuario ha iniciado sesión
+  const DEFAULT_PROFILE_PICTURE_URL = "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-profile-picture-male-icon.png";
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const handleSignInWithGoogle = async () => {
+    try {
+      signInWithGoogle();
+      setIsLoggedIn(true); // Actualiza el estado si el inicio de sesión es exitoso
+      toggleModal(); // Cierra el modal después de iniciar sesión
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSignOut = () => {
+    logOut();
+    setIsLoggedIn(false);
+    toggleModal();
+  };
+
+  return (
+    <>
+      <TouchableOpacity style={styles.profileButton} onPress={toggleModal}>
+        <Image
+          source={{ uri: DEFAULT_PROFILE_PICTURE_URL }} // Reemplaza con la URL de la imagen del usuario
+          style={styles.profileImage}
+        />
+      </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={toggleModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <TouchableOpacity onPress={toggleModal} style={styles.closeButton}>
+              <Text style={styles.closeButtonText}>X</Text>
+            </TouchableOpacity>
+            <Text style={styles.modalText}>Perfil del Usuario</Text>
+            {!isLoggedIn ? (
+              <>
+                <TouchableOpacity style={styles.authButton}>
+                  <Text style={styles.authButtonText}>Registrarse</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.authButton}>
+                  <Text style={styles.authButtonText}>Iniciar sesión</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.authButton} onPress={handleSignInWithGoogle}>
+                  <Text style={styles.authButtonText}>Iniciar con Google</Text>
+                </TouchableOpacity>
+              </>
+            ) : (
+              <>
+                <TouchableOpacity style={styles.authButton}>
+                  <Text style={styles.authButtonText}>Lista de Deseados</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.authButton} onPress={handleSignOut}>
+                  <Text style={styles.authButtonText}>Cerrar Sesión</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </View>
+        </View>
+      </Modal>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  profileButton: {
+    marginLeft: 15,
+    borderRadius: 25,
+    overflow: 'hidden',
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: 300,
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 50, // Ajusta según sea necesario
+    marginRight: 10, // Ajusta según sea necesario
+    position: 'relative',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    backgroundColor: 'transparent',
+  },
+  closeButtonText: {
+    color: '#000',
+    fontSize: 18,
+  },
+  authButton: {
+    backgroundColor: '#1DB954',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    width: '100%', // Asegura que todos los botones tengan el mismo ancho
+    alignItems: 'center', // Centra el texto dentro del botón
+  },
+  authButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  googleButton: {
+    backgroundColor: '#4285F4',
+    padding: 10,
+    borderRadius: 5,
+    width: '100%', // Asegura que todos los botones tengan el mismo ancho
+    alignItems: 'center', // Centra el texto dentro del botón
+  },
+  googleButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  loggedInText: {
+    fontSize: 18,
+    color: '#000',
+  },
+});
+
+export default LoginModal;
--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Modal, Image } from 'react-native';
-import { signInWithGoogle, logOut, auth } from '../config/firebaseConfig';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import LoginModal from './LoginModal';
 const Navbar = () => {
   const [hoveredItem, setHoveredItem] = useState<string | null>(null);
-  const [modalVisible, setModalVisible] = useState(false);
 
   const handleMouseEnter = (item: string) => {
     setHoveredItem(item);
@@ -11,10 +10,6 @@ const Navbar = () => {
 
   const handleMouseLeave = () => {
     setHoveredItem(null);
-  };
-
-  const toggleModal = () => {
-    setModalVisible(!modalVisible);
   };
 
   return (
@@ -37,31 +32,8 @@ const Navbar = () => {
             </Text>
           </TouchableOpacity>
         ))}
-        <TouchableOpacity style={styles.profileButton} onPress={toggleModal}>
-          <Image
-            source={{ uri: 'https://via.placeholder.com/150' }} // Reemplaza con la URL de la imagen del usuario
-            style={styles.profileImage}
-          />
-        </TouchableOpacity>
+        <LoginModal />
       </View>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={toggleModal}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <TouchableOpacity onPress={toggleModal} style={styles.closeButton}>
-              <Text style={styles.closeButtonText}>X</Text>
-            </TouchableOpacity>
-            <Text style={styles.modalText}>Perfil del Usuario</Text>
-            <TouchableOpacity style={styles.googleButton} onPress={signInWithGoogle}>
-              <Text style={styles.googleButtonText}>Iniciar con Google</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
     </View>
   );
 };
@@ -90,53 +62,6 @@ const styles = StyleSheet.create({
   },
   navItemHovered: {
     color: '#1DB954',
-  },
-  profileButton: {
-    marginLeft: 15,
-    borderRadius: 25,
-    overflow: 'hidden',
-  },
-  profileImage: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    width: 300,
-    padding: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    alignItems: 'center',
-    position: 'relative',
-  },
-  modalText: {
-    fontSize: 18,
-    marginBottom: 20,
-  },
-  closeButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    backgroundColor: 'transparent',
-  },
-  closeButtonText: {
-    color: '#000',
-    fontSize: 18,
-  },
-  googleButton: {
-    backgroundColor: '#4285F4',
-    padding: 10,
-    borderRadius: 5,
-  },
-  googleButtonText: {
-    color: '#fff',
-    fontSize: 16,
   },
 });
 
