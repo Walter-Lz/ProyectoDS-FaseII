b4a128e8123035618be861c9073c0ef6a4ac08ef
Kevin Jiménez <67296979+Khraben@users.noreply.github.com>
2024-10-11 13:59:34 -0600

    Update: Add Back Button and Change Visuals

--- app/(tabs)/DetailsProduct.tsx
+++ app/(tabs)/DetailsProduct.tsx
@@ -1,15 +1,16 @@
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useRoute, useNavigation } from '@react-navigation/native';
 import React, { useState, useEffect, useRef } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Dimensions, SafeAreaView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { RootStackParamList } from '../RootParametros';
 import { getCurrentUser } from '../../config/firebaseConfig';
-import { getFirestore, collection, doc, getDoc, updateDoc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, setDoc } from 'firebase/firestore';
 import { db } from '../../config/firebaseConfig';
 import { GetItem } from '../../config/ApiRequest';
 import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
 import { useTheme } from '../ThemeContext';
-// Define la interfaz para las props
+import Loading from '../Loading'; 
+
 interface Product {
   id: string;
   title: string;
@@ -27,6 +28,7 @@ const { width: viewportWidth } = Dimensions.get('window');
 const DetailsProduct: React.FC = () => {
   const { isDarkTheme } = useTheme();
   const route = useRoute<DetailsProductRouteProp>();
+  const navigation = useNavigation();
   const { idProduct } = route.params;
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
@@ -37,7 +39,7 @@ const DetailsProduct: React.FC = () => {
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const data = await GetItem(idProduct); // Llamada a la API
+        const data = await GetItem(idProduct); 
         setProduct(data);
         setLoading(false);
       } catch (error) {
@@ -55,65 +57,78 @@ const DetailsProduct: React.FC = () => {
         const user = getCurrentUser();
         if (!user) {
           return ('No user is logged in');
-        //  return;
-        }else{
-        const carritoRef = doc(db, 'Carrito', user.uid);
-        const carritoDoc = await getDoc(carritoRef);
+        } else {
+          const carritoRef = doc(db, 'Carrito', user.uid);
+          const carritoDoc = await getDoc(carritoRef);
 
-        if (carritoDoc.exists()) {
-          const carritoData = carritoDoc.data();
-          const idProducts = carritoData.id_products || [];
-          if (idProducts.includes(idProduct)) {
-            setIsWishlisted(true);
+          if (carritoDoc.exists()) {
+            const carritoData = carritoDoc.data();
+            const idProducts = carritoData.id_products || [];
+            if (idProducts.includes(idProduct)) {
+              setIsWishlisted(true);
+            } else {
+              setIsWishlisted(false);
+            }
           } else {
             setIsWishlisted(false);
           }
-        } else {
-          setIsWishlisted(false);
         }
-      }} catch (error) {
+      } catch (error) {
         console.error('Error checking wishlist status:', error);
       }
     };
 
     checkIfWishlisted();
   }, [idProduct]);
+
   const handleWishlist = async () => {
     try {
       const user = getCurrentUser();
       if (!user) {
         return ('No user is logged in');
-     //   return;
-      }else{
-      const carritoRef = doc(db, 'Carrito', user.uid);
-      const carritoDoc = await getDoc(carritoRef);
+      } else {
+        const carritoRef = doc(db, 'Carrito', user.uid);
+        const carritoDoc = await getDoc(carritoRef);
 
-      if (carritoDoc.exists()) {
-        const carritoData = carritoDoc.data();
-        const idProducts = carritoData.id_products || [];
+        if (carritoDoc.exists()) {
+          const carritoData = carritoDoc.data();
+          const idProducts = carritoData.id_products || [];
 
-        if (idProducts.includes(idProduct)) {
-          console.log('El producto ya está registrado en la lista de deseados');
+          if (idProducts.includes(idProduct)) {
+            // Eliminar el producto de la lista de deseos
+            const updatedIdProducts = idProducts.filter((id: string) => id !== idProduct);
+            await updateDoc(carritoRef, {
+              id_products: updatedIdProducts,
+            });
+            console.log('Producto eliminado de la lista de deseados');
+            setIsWishlisted(false);
+          } else {
+            // Agregar el producto a la lista de deseos
+            await updateDoc(carritoRef, {
+              id_products: [...idProducts, idProduct],
+            });
+            console.log('Producto agregado a la lista de deseados');
+            setIsWishlisted(true);
+          }
         } else {
-          await updateDoc(carritoRef, {
-            id_products: [...idProducts, idProduct],
-          });
+          await setDoc(carritoRef, {
+            id_products: [idProduct],
+          }, { merge: true });
           console.log('Producto agregado a la lista de deseados');
           setIsWishlisted(true);
         }
-      } else {
-        await setDoc(carritoRef, {
-          id_products: [idProduct],
-        }, { merge: true });
-        console.log('Producto agregado a la lista de deseados');
-        setIsWishlisted(true);
       }
-    }} catch (error) {
-      console.error('Error adding product to wishlist:', error);
+    } catch (error) {
+      console.error('Error adding/removing product to/from wishlist:', error);
     }
   };
+
+  const handleBack = () => {
+    navigation.goBack();
+  };
+
   if (loading) {
-    return <Text>Loading...</Text>;
+    return <Loading />; 
   }
   if (!product) {
     return <Text>Product not found</Text>;
@@ -122,7 +137,7 @@ const DetailsProduct: React.FC = () => {
   const imageList = product.pictures.map(picture => picture.secure_url);
   const handleThumbnailPress = (index: number) => {
     if (carouselRef.current) {
-      carouselRef.current.scrollTo({ index, animated: true }); // Desplazar el carrusel
+      carouselRef.current.scrollTo({ index, animated: true }); 
       setCurrentIndex(index);
     }
   };
@@ -131,12 +146,15 @@ const DetailsProduct: React.FC = () => {
   };
 
   return (
-    <SafeAreaView style={isDarkTheme ? styles.safeAreaDark :styles.safeArea}>
-      <ScrollView style={isDarkTheme ? styles.containerDark:styles.container}>
-        <TouchableOpacity style={isDarkTheme ? styles.wishlistButtonDark :styles.wishlistButton} onPress={handleWishlist}>
-          <Icon name={isWishlisted ? 'heart' : 'heart-o'} size={30} color={isDarkTheme ? '#FFDD00':'#900' } />
-        </TouchableOpacity>
-        <View style={styles.imageSection}>
+    <SafeAreaView style={isDarkTheme ? styles.safeAreaDark : styles.safeArea}>
+      <ScrollView style={isDarkTheme ? styles.containerDark : styles.container}>
+        <View style={styles.backButtonContainer}>
+          <TouchableOpacity style={isDarkTheme ? styles.backButtonDark : styles.backButton} onPress={handleBack}>
+            <Icon name="arrow-left" size={30} color={isDarkTheme ? '#FFDD00' : '#3483FA'} />
+          </TouchableOpacity>
+        </View>
+        <Text style={isDarkTheme ? styles.titleDark : styles.title}>{product.title}</Text>
+        <View style={styles.galleryContainer}>
           <Image
             source={{ uri: imageList[currentIndex] }}
             style={styles.image}
@@ -155,21 +173,23 @@ const DetailsProduct: React.FC = () => {
             autoFillData={false}
           />
         </View>
-        <ScrollView horizontal style={styles.thumbnailSection}>
+        <ScrollView horizontal style={styles.thumbnailSection} contentContainerStyle={styles.thumbnailSectionContent}>
           {imageList.map((image, index) => (
             <TouchableOpacity key={index} onPress={() => handleThumbnailPress(index)}>
               <Image source={{ uri: image }} style={styles.thumbnail} />
             </TouchableOpacity>
           ))}
         </ScrollView>
+        <TouchableOpacity style={isDarkTheme ? styles.wishlistButtonDark : styles.wishlistButton} onPress={handleWishlist}>
+          <Icon name={isWishlisted ? 'heart' : 'heart-o'} size={30} color={isDarkTheme ? '#FFDD00' : '#3483FA'} />
+        </TouchableOpacity>
         <View style={styles.detailsSection}>
-          <View style={isDarkTheme ? styles.titleAreaDark :styles.titleArea}>
-            <Text style={isDarkTheme ? styles.titleDark :styles.title}>{product.title}</Text>
-            <Text style={isDarkTheme ?styles.objectiveTitleDark :styles.objectiveTitle}>Estado del producto: {product.condition}</Text>
-            <Text style={isDarkTheme ?styles.objectiveTitleDark :styles.objectiveTitle}>Descripción</Text>
-            <Text style={isDarkTheme ?styles.objectiveTitleDark :styles.objectiveTitle}>Precio del Producto: {product.price ? formatPrice(product.price) : "No establecido"}</Text>
-            <Text style={isDarkTheme ?styles.objectiveTitleDark :styles.objectiveTitle}>Cantidad Disponible: {product.initial_quantity}</Text>
-            <Text style={isDarkTheme ?styles.objectiveTitleDark :styles.objectiveTitle}>{product.warranty ? product.warranty : "Sin garantía"}</Text>
+          <View style={isDarkTheme ? styles.titleAreaDark : styles.titleArea}>
+            <Text style={isDarkTheme ? styles.objectiveTitleDark : styles.objectiveTitle}>Estado del producto: {product.condition}</Text>
+            <Text style={isDarkTheme ? styles.objectiveTitleDark : styles.objectiveTitle}>Descripción</Text>
+            <Text style={isDarkTheme ? styles.objectiveTitleDark : styles.objectiveTitle}>Precio del Producto: {product.price ? formatPrice(product.price) : "No establecido"}</Text>
+            <Text style={isDarkTheme ? styles.objectiveTitleDark : styles.objectiveTitle}>Cantidad Disponible: {product.initial_quantity}</Text>
+            <Text style={isDarkTheme ? styles.objectiveTitleDark : styles.objectiveTitle}>{product.warranty ? product.warranty : "Sin garantía"}</Text>
           </View>
         </View>
       </ScrollView>
@@ -189,42 +209,48 @@ const styles = StyleSheet.create({
     backgroundColor: '#333',
   },
   container: {
-    padding: 20,
-    maxWidth: 900,
+    padding: 10,
     backgroundColor: '#fff',
-    marginLeft: 'auto',
-    marginRight: 'auto',
     flex: 1,
   },
   containerDark: {
-    padding: 20,
-    maxWidth: 900,
+    padding: 10,
     backgroundColor: '#333',
-    marginLeft: 'auto',
-    marginRight: 'auto',
     flex: 1,
   },
-
-  imageSection: {
+  galleryContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop:-5,
-    marginBottom: 10,
-    marginRight: -30,
-    marginLeft:0,
+    marginVertical: 10,
     width: '100%',
     height: 300,
-    
+  },
+  backButtonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  backButton: {
+    zIndex: 1,
+  },
+  backButtonDark: {
+    zIndex: 1,
   },
   thumbnailSection: {
     flexDirection: 'row',
-    marginBottom: 40,
+    marginBottom: 10, 
+  },
+  thumbnailSectionContent: {
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    flexGrow: 1, 
   },
   thumbnail: {
     width: 80,
     height: 80,
     marginRight: 10,
     borderRadius: 10,
+    marginBottom: 0, 
   },
   image: {
     width: '100%',
@@ -233,9 +259,9 @@ const styles = StyleSheet.create({
     shadowColor: 'rgba(0, 0, 0, 0.2)',
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.8,
+    marginBottom: 0, 
   },
   detailsSection: {
-    minHeight: '60%',
     flex: 1,
   },
   titleArea: {
@@ -244,6 +270,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     marginBottom: 20,
+    paddingHorizontal: 10,
   },
   titleAreaDark: {
     display: 'flex',
@@ -251,6 +278,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#444',
     alignItems: 'center',
     marginBottom: 20,
+    paddingHorizontal: 10,
   },
   title: {
     fontSize: 24,
@@ -277,18 +305,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   wishlistButton: {
-    color: '#3483FA',
-    position: 'absolute',
-    top: 20,
-    right: 20,
-    zIndex: 1,
+    alignSelf: 'center',
+    marginVertical: 20,
   },
   wishlistButtonDark: {
-    color: '#FFDD00',
-    position: 'absolute',
-    top: 20,
-    right: 20,
-    zIndex: 1,
+    alignSelf: 'center',
+    marginVertical: 20,
   },
-
 });
